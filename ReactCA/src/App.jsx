bf612a84c9e420bb5aa62fbc91bc53ba40d46de8
Veronika Aas <veronikaaas@live.no>
2025-03-03 17:38:08 +0100

    feat: add footer to router

--- ReactCA/src/App.jsx
+++ ReactCA/src/App.jsx
@@ -1,27 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 import Navbar from './components/header/Navbar';
 import Home from './views/1.home/Home';
 import Single from './views/2.singleproduct/Single';
 import Checkout from './views/3.checkout/Checkout';
 import Success from './views/4.checkoutsuccess/Success';
 import Contact from './views/5.contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Router>
         <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/single" element={<Single />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {/* flex-grow gjør at main tar opp all ledig plass */}
+        <main className="flex-grow">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/single" element={<Single />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
