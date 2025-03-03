ceae80b3edc61962b417c9485973f2752d9f6cc3
Veronika Aas <veronikaaas@live.no>
2025-03-03 18:05:10 +0100

    feat: add cartprovider

--- ReactCA/src/App.jsx
+++ ReactCA/src/App.jsx
@@ -7,24 +7,27 @@ import Checkout from './views/3.checkout/Checkout';
 import Success from './views/4.checkoutsuccess/Success';
 import Contact from './views/5.contact/Contact';
 import Footer from './components/footer/Footer';
+import { CartProvider } from './components/cart/Cartcontext';
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Router>
-        <Navbar />
-        {/* flex-grow gjør at main tar opp all ledig plass */}
-        <main className="flex-grow">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/single" element={<Single />} />
-            <Route path="/success" element={<Success />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
+      <CartProvider>
+        <Router>
+          <Navbar />
+          {/* flex-grow gjør at main tar opp all ledig plass */}
+          <main className="flex-grow">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/single" element={<Single />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
+      </CartProvider>
     </div>
   );
 };
