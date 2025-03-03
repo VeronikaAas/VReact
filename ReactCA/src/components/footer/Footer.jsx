e61bb32948ab6b3e2908878d5bc348b71ca74184
Veronika Aas <veronikaaas@live.no>
2025-03-03 16:26:42 +0100

    build: components

---
+++ ReactCA/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="bg-gray-800 text-white p-4 text-center">
+                <p>&copy; 2021 ReactCA</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ ReactCA/src/components/header/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="bg-gray-800 p-4">
+                <h1 className="text-white text-2xl">ReactCA</h1>
+            </nav>
+        </div>
+    )
+}
