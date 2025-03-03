2de7bfc0b50d87ac4579842b0173cff9cfe3e6e9
Veronika Aas <veronikaaas@live.no>
2025-03-03 16:32:17 +0100

    build: template for building pages

---
+++ ReactCA/src/views/2.singleproduct/Single.jsx
@@ -0,0 +1,11 @@
+
+
+const Single = () => {
+  return (
+    <div>
+      <h1>Single Product</h1>
+    </div>
+  );
+};
+
+export default Single;
---
+++ ReactCA/src/views/4.checkoutsuccess/Success.jsx
@@ -0,0 +1,12 @@
+
+
+const Success = () => {
+    return (
+        <div>
+            <h1>Thank you for your purchase!</h1>
+            <p>We appreciate your business and hope you enjoy your new products.</p>
+        </div>
+    )
+}
+
+export default Success;
