4908e4076d7c82b2c7007ab31c46a3e850718be6
Veronika Aas <veronikaaas@live.no>
2025-03-06 20:30:00 +0100

    feat: add possibility to add and remove items

--- ReactCA/src/components/cart/Cartcontext.jsx
+++ ReactCA/src/components/cart/Cartcontext.jsx
@@ -17,19 +17,37 @@ export const CartProvider = ({ children }) => {
     }, [cart]);
 
     const addToCart = (product) => {
-        setCart((prevCart) => [...prevCart, product]);
+        setCart((prevCart) => {
+            const existingItem = prevCart.find((item) => item.id === product.id);
+            if (existingItem) {
+                return prevCart.map((item) =>
+                    item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+                );
+            } else {
+                return [...prevCart, { ...product, quantity: 1 }];
+            }
+        });
     };
 
     const removeFromCart = (productId) => {
+        setCart((prevCart) => {
+            const existingItem = prevCart.find((item) => item.id === productId);
+            if (existingItem.quantity === 1) {
+                return prevCart.filter((item) => item.id !== productId);
+            } else {
+                return prevCart.map((item) =>
+                    item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
+                );
+            }
+        });
+    };
+
+    const deleteFromCart = (productId) => {
         setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
     };
 
-    const clearCart = () => {
-        setCart([]);
-    };
-
     return (
-        <CartContext.Provider value={{ cart, setCart, addToCart, removeFromCart, clearCart }}>
+        <CartContext.Provider value={{ cart, setCart, addToCart, removeFromCart, deleteFromCart }}>
             {children}
         </CartContext.Provider>
     );
@@ -38,7 +56,7 @@ export const CartProvider = ({ children }) => {
 export const useCart = () => {
     const context = useContext(CartContext);
     if (!context) {
-        throw new Error("useCart må brukes inne i en CartProvider");
+        throw new Error("useCart must be used within a CartProvider");
     }
     return context;
 };
