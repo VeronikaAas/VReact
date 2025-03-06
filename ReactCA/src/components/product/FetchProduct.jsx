6055d13157209444e8e9cea7c3d039f88f044ef6
Veronika Aas <veronikaaas@live.no>
2025-03-06 20:30:58 +0100

    feat: add a button with add item to cart

--- ReactCA/src/components/product/FetchProduct.jsx
+++ ReactCA/src/components/product/FetchProduct.jsx
@@ -1,23 +1,24 @@
 import { Link } from "react-router-dom";
+import { useCart } from '../cart/Cartcontext';
 
-const Product = ({ product, addToCart }) => {
+const Product = ({ product }) => {
+    const { addToCart } = useCart(); 
+
     if (!product || !product.id) {
         return null;
     }
-
     const imageUrl = product.image?.url || "https://via.placeholder.com/300";
     const hasDiscount = product.discountedPrice && product.discountedPrice < product.price;
 
     return (
         <div className="bg-sky-300 border border-sky-500 p-4 rounded-lg shadow-lg w-full min-h-[280px]">
             <div className="bg-white p-4 rounded-lg shadow-md text-black">
-
                 <h2 className="text-lg font-semibold text-center">{product.title}</h2>
 
                 <Link to={`/product/${product.id}`} className="flex justify-center">
                     <img
                         src={imageUrl}
-                        alt={product.image?.alt || "Produktbilde"}
+                        alt={product.image?.alt || "Product image"}
                         className="w-52 h-52 object-cover rounded cursor-pointer"
                     />
                 </Link>
@@ -37,18 +38,18 @@ const Product = ({ product, addToCart }) => {
                 {/* Buttons */}
                 <div className="flex justify-between w-full mt-4 gap-x-2">
                     <Link to={`/product/${product.id}`} className="w-1/2 text-center">
-                        <button className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-800 w-full cursor-pointer">
+                        <button aria-label="View product"
+                         className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-800 w-full cursor-pointer">
                             View product
                         </button>
                     </Link>
 
-                    <button
+                    <button aria-label="Add to cart"
                         onClick={() => addToCart(product)}
                         className="bg-emerald-500 text-white px-4 py-2 rounded hover:bg-emerald-800 w-1/2 cursor-pointer">
                         Add to Cart
                     </button>
                 </div>
-
             </div>
         </div>
     );
