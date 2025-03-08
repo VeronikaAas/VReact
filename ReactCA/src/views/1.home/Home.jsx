a2a039190dcff4c9f52a076b4aea357f32276d24
Veronika Aas <veronikaaas@live.no>
2025-03-08 12:34:02 +0100

    feat: change to 1 grid on mobilephones

--- ReactCA/src/views/1.home/Home.jsx
+++ ReactCA/src/views/1.home/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
             </Helmet>
             <SearchBar products={products} />
 
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 flex-grow">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6 flex-grow">
                 {products.map((product) => (
                     <Product key={product.id} product={product} addToCart={addToCart} />
                 ))}
